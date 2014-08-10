2934306faacc5ea9c14f889e207d52066ba4e2d2
conor22 <conorpokane@gmail.com>
2014-08-10 14:27:02 +0100

    Latest versions

---
+++ plot1.R
@@ -0,0 +1,31 @@
+## Exploratory Data Analysis
+## Project 1: Plot 1
+
+## Please ensure the "household_power_consumption.txt" dataset is in your working directory before starting
+## The data can be downloaded from here [https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip]
+
+## Read the data into memory
+power <- read.table("household_power_consumption.txt", header=T, sep = ";", colClasses="character", na.strings="?")
+
+## Convert the variables into the required classes
+power$Time <- strptime(paste(power$Date,power$Time), "%d/%m/%Y %H:%M:%S")
+power$Date <- as.Date(power$Date,"%d/%m/%Y")
+
+
+## Subset the data based on date paramters required  
+sub_power <- subset(power, power$Date %in% as.Date(c("2007-02-01","2007-02-02")))
+
+## Convert the rest if the variables into numerics 
+sub_power[,3] <- as.numeric(sub_power[,3])
+sub_power[,4] <- as.numeric(sub_power[,4])
+sub_power[,5] <- as.numeric(sub_power[,5])
+sub_power[,6] <- as.numeric(sub_power[,6])
+sub_power[,7] <- as.numeric(sub_power[,7])
+sub_power[,8] <- as.numeric(sub_power[,8])
+sub_power[,9] <- as.numeric(sub_power[,9])
+
+## Ok - we should now have a correctly formatted and clean data set to plot on
+## Create plot1
+png(filename="plot1.png", width=480, height=480, units="px")
+hist(sub_power[,3], col="red", main = "Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+dev.off()
